d6112b9f983db9332e297fab2313996cf633c79c
Thaquidheen M A <thaquidheen@gmail.com>
2023-10-05 11:49:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './component/Header'
+import Footer from './component/Footer'
+import HomeScreen from './pages/HomeScreen';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import ProductScreen from './pages/ProductScreen';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <Header />
+    <div className='py-3'>
+      <Container>
+      <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen/>}  />
+          </Routes>
+      </Container>
     </div>
+    <Footer />
+  </Router>
   );
 }
 
---
+++ src/component/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" variant='dark' className="bg-dark" collapseOnSelect>
+      <Container fluid>
+        <Navbar.Brand href="/">Proshop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="mr-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
